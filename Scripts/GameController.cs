d4624b949ac873800933e23fcea8eae5333e9d6d
OsamaQHasan <106831935+OsamaQHasan@users.noreply.github.com>
2024-08-12 14:20:48 +0400

    Went back to basics

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -3,27 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     GameObject[] lights;
-
+    [SerializeField] Color playerColor, backgroundColor, groundColor;
+    [SerializeField] Color[] lightColors;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject background;
+    [SerializeField] Material groundMaterial;
     [SerializeField] float totalIntensity;
-
     // Start is called before the first frame update
     void Start()
     {
-        SetupStage();
-
-
+        int count = 0;
+        lights = GameObject.FindGameObjectsWithTag("LightSource");
+        foreach (var light in lights)
+        {
+            light.transform.GetComponentInChildren<Light2D>().intensity = totalIntensity / lights.Length;
+            light.gameObject.GetComponent<SpriteRenderer>().color = lightColors[count];
+            count = (count + 1) % lightColors.Length;
+        }
+        player.GetComponent<SpriteRenderer>().color = playerColor;
+        background.GetComponent<SpriteRenderer>().color = backgroundColor;
+        groundMaterial.color = groundColor;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void ResetStage(InputAction.CallbackContext context)
     {
@@ -31,17 +43,5 @@ public class GameController : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
-    }
-    void SetupStage()
-    {
-        lights = GameObject.FindGameObjectsWithTag("LightSource");
-        Light2D globalLight = GameObject.FindGameObjectWithTag("GlobalLight").GetComponent<Light2D>();
-        globalLight.intensity = totalIntensity / 2;
-        foreach (var light in lights)
-        {
-            light.transform.GetComponentInChildren<Light2D>().intensity = (totalIntensity /2) / lights.Length;
-     
-        }
-   
     }
 }
